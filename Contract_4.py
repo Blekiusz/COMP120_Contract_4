00ad557bb7031e34972001b797580f5c17533541
Konrad <kowalewskikonrad@o2.pl>
2018-11-22 12:16:15 +0000

    Added sound variation

--- Contract_4.py
+++ Contract_4.py
@@ -15,43 +15,85 @@ pygame.display.set_mode((200, 200))
 
 
 class UIAudioGenerator:
-    OUTPUT_FILENAME = "noise.wav"
-    LENGTH_OF_FILE_IN_SECONDS = 0.2
+    output_filenames = ["start_game.wav", "options.wav", "quit.wav"]
+    sound_characteristic = ["positive", "neutral", "negative"]
+    length_of_file_in_seconds = [0.5, 0.2, 0.8]
+
     CHANNEL_COUNT = 1
     SAMPLE_WIDTH = 2  # 2 bytes per sample
     SAMPLE_RATE = 44100
-    SAMPLE_LENGTH = int(SAMPLE_RATE * LENGTH_OF_FILE_IN_SECONDS)
     COMPRESSION_TYPE = 'NONE'
     COMPRESSION_NAME = 'not compressed'
     MAX_VALUE = 32767
     FREQUENCY = 1500
     VOLUME = 0.5
+    default_frequency = 1500
 
-    noise_out = wave.open(OUTPUT_FILENAME, 'w')
-    noise_out.setparams((CHANNEL_COUNT, SAMPLE_WIDTH, SAMPLE_RATE,
-    SAMPLE_LENGTH, 'NONE', 'not compressed'))
+    def sound_preparation(self):
+        for i in range(len(self.output_filenames)):
+            sound_output = wave.open(self.output_filenames[i], 'w')
 
+            sample_length = int(self.SAMPLE_RATE * self.length_of_file_in_seconds[i])
 
-    values = []
+            sound_characteristic = self.sound_characteristic[i]
 
-    def create_sound(self):
-        for i in range(0, self.SAMPLE_LENGTH):
-            if i > self.SAMPLE_LENGTH * 0.25:
-                if self.FREQUENCY < 3000:
-                    self.FREQUENCY += 0.1
-            if i < self.SAMPLE_LENGTH * 0.2:
-                self.FREQUENCY = random.randint(1000, 3000)
+            sound_output.setparams((self.CHANNEL_COUNT, self.SAMPLE_WIDTH,
+                                    self.SAMPLE_RATE, sample_length,
+                                    self.COMPRESSION_TYPE, self.COMPRESSION_NAME)
+                                   )
+
+            self.create_sound(UIAudioGenerator, sound_output,
+                              sample_length, sound_characteristic
+                              )
+
+    def positive_sound(self, sample_length, loop_value):
+
+        if loop_value > sample_length * 0.25:
+            if self.FREQUENCY < 3000:
+                self.FREQUENCY += 0.1
+
+    def neutral_sound(self, sample_length, loop_value):
+
+        if loop_value < sample_length * 0.3:
+            self.FREQUENCY = 700
+        elif loop_value < sample_length * 0.6:
+            self.FREQUENCY = 1100
+        elif loop_value < sample_length * 1:
+            self.FREQUENCY = 700
+
+    def negative_sound(self, sample_length, loop_value):
+        if loop_value < sample_length * 0.3:
+            self.FREQUENCY = 2000
+        elif loop_value < sample_length * 0.5:
+            self.FREQUENCY = 600
+        else:
+            self.FREQUENCY = 200
+
+    def create_sound(self, sound_output, sample_length, sound_characteristic):
+        self.FREQUENCY = self.default_frequency
+        values = []
+        for i in range(0, sample_length):
+            if sound_characteristic == "positive":
+                self.positive_sound(UIAudioGenerator, sample_length, i)
+            if sound_characteristic == "neutral":
+                self.neutral_sound(UIAudioGenerator, sample_length, i)
+            if sound_characteristic == "negative":
+                self.negative_sound(UIAudioGenerator, sample_length, i)
+
             value = math.sin(2.0 * math.pi * self.FREQUENCY * (i / self.SAMPLE_RATE)) * (self.VOLUME * self.MAX_VALUE)
             packed_value = struct.pack('h', int(value))
             for j in range(0, self.CHANNEL_COUNT):
-                self.values.append(packed_value)
+                values.append(packed_value)
 
-        self.noise_out.writeframes(b''.join(self.values))
-        self.noise_out.close()
+        sound_output.writeframes(b''.join(values))
+        sound_output.close()
 
 
-UIAudioGenerator.create_sound(UIAudioGenerator)
-sound = pygame.mixer.Sound(UIAudioGenerator.OUTPUT_FILENAME)
+UIAudioGenerator.sound_preparation(UIAudioGenerator)
+
+sound = pygame.mixer.Sound(UIAudioGenerator.output_filenames[0])
+sound1 = pygame.mixer.Sound(UIAudioGenerator.output_filenames[1])
+sound2 = pygame.mixer.Sound(UIAudioGenerator.output_filenames[2])
 
 
 def main():
@@ -64,6 +106,12 @@ def main():
                 if event.key == K_1:
                     sound.stop()
                     sound.play()
+                if event.key == K_2:
+                    sound1.stop()
+                    sound1.play()
+                if event.key == K_3:
+                    sound2.stop()
+                    sound2.play()
 
 
 if __name__ == "__main__":
