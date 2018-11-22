336a18ad2f2c269b6dc0d228751d8e29c0363398
Konrad <kowalewskikonrad@o2.pl>
2018-11-22 15:30:42 +0000

    Satisfied pep257 standard

--- Contract_4.py
+++ Contract_4.py
@@ -1,13 +1,15 @@
-"""Tinkering audio contract4 by:
-Konrad Kowalewski - main Driver
-Joachim Rayski - Navigator and occasional Driver
+"""
+Tinkering Audio - Contract 4.
+
+by:
+Konrad Kowalewski - Driver
+Joachim Rayski - Navigator and Occasional Driver
 """
 import wave
 import struct
 import math
 import pygame
 import sys
-import random
 from pygame.locals import *
 
 pygame.init()
@@ -15,6 +17,8 @@ pygame.display.set_mode((200, 200))
 
 
 class UIAudioGenerator:
+    """Class for a whole script."""
+
     output_filenames = ["start_game.wav", "options.wav", "quit.wav"]
     sound_characteristic = ["positive", "neutral", "negative"]
     length_of_file_in_seconds = [0.5, 0.2, 0.8]
@@ -30,16 +34,24 @@ class UIAudioGenerator:
     default_frequency = 1500
 
     def sound_preparation(self):
+        """
+        Script sets up files and passes them to another script.
+
+        :return: No return.
+        """
         for i in range(len(self.output_filenames)):
             sound_output = wave.open(self.output_filenames[i], 'w')
 
-            sample_length = int(self.SAMPLE_RATE * self.length_of_file_in_seconds[i])
+            sample_length = int(self.SAMPLE_RATE *
+                                self.length_of_file_in_seconds[i]
+                                )
 
             sound_characteristic = self.sound_characteristic[i]
 
             sound_output.setparams((self.CHANNEL_COUNT, self.SAMPLE_WIDTH,
                                     self.SAMPLE_RATE, sample_length,
-                                    self.COMPRESSION_TYPE, self.COMPRESSION_NAME)
+                                    self.COMPRESSION_TYPE,
+                                    self.COMPRESSION_NAME)
                                    )
 
             self.create_sound(UIAudioGenerator, sound_output,
@@ -47,13 +59,25 @@ class UIAudioGenerator:
                               )
 
     def positive_sound(self, sample_length, loop_value):
+        """
+        Function makes sound sound friendly and positive.
 
+        :param sample_length: Used to check at what stage sound modification is
+        :param loop_value: Shows what point script is at
+        :return: Modifies frequency value
+        """
         if loop_value > sample_length * 0.25:
             if self.FREQUENCY < 3000:
                 self.FREQUENCY += 0.1
 
     def neutral_sound(self, sample_length, loop_value):
+        """
+        Function makes neutral feedback sound.
 
+        :param sample_length: Used to check at what stage sound modification is
+        :param loop_value: Shows what point script is at
+        :return: Modifies frequency value
+        """
         if loop_value < sample_length * 0.3:
             self.FREQUENCY = 700
         elif loop_value < sample_length * 0.6:
@@ -62,26 +86,49 @@ class UIAudioGenerator:
             self.FREQUENCY = 700
 
     def negative_sound(self, sample_length, loop_value):
-        if loop_value < sample_length * 0.3:
+        """
+        Function makes negative, decline like sound.
+
+        :param sample_length: Used to check at what stage sound modification is
+        :param loop_value: Shows what point script is at
+        :return: Modifies frequency value
+        """
+        length_of_first_note = sample_length * 0.3
+        length_of_second_note = sample_length * 0.5
+
+        if loop_value < length_of_first_note:
             self.FREQUENCY = 2000
-        elif loop_value < sample_length * 0.5:
+        elif loop_value < length_of_second_note:
             self.FREQUENCY = 600
         else:
             self.FREQUENCY = 200
 
     def create_sound(self, sound_output, sample_length, sound_characteristic):
+        """
+        Function that creates sound and exports sound file.
+
+        :param sound_output: Reference to a sound file.
+        :param sample_length: Length of certain sound file.
+        :param sound_characteristic: Characteristic of certain sound file.
+        :return: Function exports sound files.
+        """
         self.FREQUENCY = self.default_frequency
         values = []
+
         for i in range(0, sample_length):
             if sound_characteristic == "positive":
                 self.positive_sound(UIAudioGenerator, sample_length, i)
-            if sound_characteristic == "neutral":
-                self.neutral_sound(UIAudioGenerator, sample_length, i)
-            if sound_characteristic == "negative":
+            elif sound_characteristic == "negative":
                 self.negative_sound(UIAudioGenerator, sample_length, i)
+            elif sound_characteristic == "neutral":
+                self.neutral_sound(UIAudioGenerator, sample_length, i)
 
-            value = math.sin(2.0 * math.pi * self.FREQUENCY * (i / self.SAMPLE_RATE)) * (self.VOLUME * self.MAX_VALUE)
+            value = math.sin(2.0 * math.pi * self.FREQUENCY *
+                             (i / self.SAMPLE_RATE)) * (self.VOLUME *
+                                                        self.MAX_VALUE)
+
             packed_value = struct.pack('h', int(value))
+
             for j in range(0, self.CHANNEL_COUNT):
                 values.append(packed_value)
 
@@ -97,6 +144,7 @@ sound2 = pygame.mixer.Sound(UIAudioGenerator.output_filenames[2])
 
 
 def main():
+    """Main loop of the game."""
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
